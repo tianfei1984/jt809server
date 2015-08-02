7ef513ea2502c04bbb29487baf2fc8d66d001b0f
tianfei1984 <tianfei-1984@163.com>
2015-08-02 10:50:33 +0800

    等待下级平台应答

--- jt809server/src/com/ltmonitor/jt809/protocol/receive/DownCtrlMsgTakeTravelReq.java
+++ jt809server/src/com/ltmonitor/jt809/protocol/receive/DownCtrlMsgTakeTravelReq.java
@@ -1,15 +1,18 @@
 package com.ltmonitor.jt809.protocol.receive;
 
 import org.apache.log4j.Logger;
-
 import com.ltmonitor.entity.TerminalCommand;
 import com.ltmonitor.jt809.app.ServiceLauncher;
 import com.ltmonitor.jt809.app.T809Manager;
 import com.ltmonitor.jt809.model.JT809Message;
 import com.ltmonitor.jt809.protocol.IReceiveProtocol;
-import com.ltmonitor.jt809.tool.Tools;
 import com.ltmonitor.service.JT808Constants;
 
+/**
+ * 上报车辆行驶记录请求消息
+ * @author tianfei
+ *
+ */
 public class DownCtrlMsgTakeTravelReq implements IReceiveProtocol {
 	private Logger logger = Logger.getLogger(DownCtrlMsgTakeTravelReq.class);
 
@@ -33,12 +36,12 @@ public class DownCtrlMsgTakeTravelReq implements IReceiveProtocol {
 			logger.error(ex.getMessage(), ex);
 		}
 		
-		byte result = 1;// 处理完毕, 对于报警督办，自动进行应答，并录入数据库
-		String txtContexnt = "1234455,223";
-		//Tools.(result);
-		byte[] tempBytes = new byte[20];
-		T809Manager.UpCtrlMsgTakeTravelAck(message.getPlateNo(),
-				message.getPlateColor(), (byte)cmdType, tempBytes);
+//		byte result = 1;// 处理完毕, 对于报警督办，自动进行应答，并录入数据库
+//		String txtContexnt = "1234455,223";
+//		//Tools.(result);
+//		byte[] tempBytes = new byte[20];
+//		T809Manager.UpCtrlMsgTakeTravelAck(message.getPlateNo(),
+//				message.getPlateColor(), (byte)cmdType, tempBytes);
 
 		return "";
 	}
